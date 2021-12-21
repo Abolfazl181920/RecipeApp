2b5f71ea17dd684e65f85fc6e2124fc125d584fa
Abolfazl <abolfazlm181920@gmail.com>
2021-12-21 23:55:21 +0330

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = "64831bc3";
+  const APP_KEY = "8dba4b49296f6e8d7a96c1151083bd55";
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <form className='search-form'>
+        <input className='search-bar' type='text' />
+        <button className='search-button' type='submit'>Search</button>
+      </form>
+    </div>
   );
 }
 
