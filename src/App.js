05e99e696b34df7e468275396d2ef5ec7ea520dc
Abolfazl <abolfazlm181920@gmail.com>
2021-12-22 00:08:20 +0330

    get data and map it in page

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const App = () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
+    console.log(data.hits);
   }
 
   return (
@@ -25,6 +26,11 @@ const App = () => {
         <input className='search-bar' type='text' />
         <button className='search-button' type='submit'>Search</button>
       </form>
+
+      {recipes.map(recipe => (
+        <Recipe />
+      ))}
+
     </div>
   );
 }
